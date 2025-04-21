79629b2aff8a78e11940797545072fe2558250aa
Sagecheni <sagecheni@outlook.com>
2025-04-22 02:08:44 +0800

    ready to cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  reactStrictMode: true,
+  // -- 添加以下配置 --
+  // 确保开启静态导出
+  output: 'export', 
+  images: {
+     unoptimized: true // 静态导出需要禁用 Next.js 图片优化
+  },
 };
 
 export default withMDX(config);
